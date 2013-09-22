968b334da8ad9ae63e1e8c6a4dd5606166d1e9da
caiguanhao <caiguanhao@gmail.com>
2013-09-23 01:04:09 +0800

    add iealert;

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,2 +1,11 @@
 //= require jquery-1.10.1.min
 //= require_tree .
+$(function(){
+  $("body").iealert({
+    support: 'ie8',
+    title: '你的浏览器太旧了，无法正常显示网页内容。',
+    text: '你现在使用的IE浏览器长期无更新，不安全，速度慢，不支持新的网页标准。强烈建议你升级、安装最新版的浏览器，以便正常浏览网站内容。',
+    upgradeTitle: '选择新版浏览器下载',
+    upgradeLink: 'http://whatbrowser.org/'
+  });
+});
